b8f5d66c07e83bed731fa2d4d6121eda5824dcff
KirbyPatrick <pkirbyinatx@gmail.com>
2019-09-09 20:07:28 -0500

    set up React Router framework

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import Navbar from "./components/Navbar";
+import Cart from "./components/Cart";
+import Details from "./components/Details";
+import ProductList from "./components/ProductList";
+import Default from "./components/Default";
+
 function App() {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-6">col number one</div>
-        <div className="col-6">
-          <span>
-            <i className="fas fa-home"></i>
-          </span>
-        </div>
-      </div>
-    </div>
+    <React.Fragment>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={ProductList} />
+        <Route path="/details" component={Details} />
+        <Route path="/cart" component={Cart} />
+        <Route component={Default} />
+      </Switch>
+    </React.Fragment>
   );
 }
 
