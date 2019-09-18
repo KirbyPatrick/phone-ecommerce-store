23a1bd3af697fd5e64f19a4b91652a1129d60754
KirbyPatrick <pkirbyinatx@gmail.com>
2019-09-18 11:04:31 -0500

    added storeProducts to state

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,18 +1,23 @@
 import React, { Component } from "react";
 import Product from "./Product";
 import Title from "./Title";
+import { storeProducts } from '../data'
 
 class ProductList extends Component {
   state = {
-    products: []
+    products: storeProducts
   };
 
   render() {
+
+    console.log(this.state.products);
+
     return (
       <React.Fragment>
         <div className="py-5">
           <div className="container">
             <Title name="our" title="products" />
+            
             <div className="row"></div>
           </div>
         </div>
