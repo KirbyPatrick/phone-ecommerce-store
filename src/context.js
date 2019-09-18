31bf6c92a0ec03de13d94c5cabf65cc32943704f
KirbyPatrick <pkirbyinatx@gmail.com>
2019-09-18 11:35:37 -0500

    added the context API into context.js. wrapped the app in the ProductProvider in index.js

---
+++ src/context.js
@@ -0,0 +1,24 @@
+// Context API to allow our state of products: storeProducts to be accessable throughout
+// the app without needing prop drilling. Similar to Redux.  
+// https://www.carlrippon.com/wp-content/uploads/2018/04/prop-drilling-v-context.png
+
+import React, { Component } from 'react';
+//above is creating a context object.  Context objects come with 2 components:
+//Provider - provides all the information for all the application.  Provider is set up on the top of the application
+//Consumer - whenever we want to use the infomation the provider gives, we use the consumer object. This way we don't have to pass props through components to get them to child components.
+
+const ProductContext = React.createContext()
+
+class ProductProvider extends Component {
+  render() {
+    return (
+      <ProductContext.Provider value="hello from context">
+        {this.props.children}
+      </ProductContext.Provider>
+    );
+  }
+}
+
+const ProductConsumer = ProductContext.Consumer;
+
+export {ProductProvider, ProductConsumer};
